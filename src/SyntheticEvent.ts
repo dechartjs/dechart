dc5229d68e2ba56d273bafe48a92df9ac4b8b86b
matt-sungwook <hisw0509@gmail.com>
2018-11-06 17:07:28 +0900

    feat: switch event-related codes from js to ts
    Fixes https://github.com/dechartjs/dechart/issues/8

--- src/SyntheticEvent.ts
+++ src/SyntheticEvent.ts
@@ -1,15 +1,26 @@
-export default function SyntheticEvent({
+function SyntheticEvent({
   bulbX = [],
   bulbY = [],
   clientX = 0,
   clientY = 0,
   selectedData,
   xIdx = 0,
-}) {
+}: SyntheticEventProps) {
   this.bulbX = bulbX;
   this.bulbY = bulbY;
   this.clientX = clientX;
   this.clientY = clientY;
   this.selectedData = selectedData;
   this.xIdx = xIdx;
+}
+
+export default SyntheticEvent;
+
+interface SyntheticEventProps {
+  bulbX: number[],
+  bulbY: number[],
+  clientX: number,
+  clientY: number,
+  selectedData?: any,
+  xIdx?: number,
 }
